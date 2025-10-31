a7f8f6757c00db604498718babbc86cc96888d36
strategicstrike <strategicstriketrading@gmail.com>
2025-10-31 20:27:43 +0530

    Delete script.js
    Signed-off-by: strategicstrike <strategicstriketrading@gmail.com>

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Thank you for contacting Strategic Strike! We'll reach out soon.");
-  this.reset();
-});
